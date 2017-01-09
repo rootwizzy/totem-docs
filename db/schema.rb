a0581a2cfc7db20a205b9e658e425cc2a682e5d9
Zachary Johnson <rootwizzy@gmail.com>
2017-01-09 15:58:59 -0600

    [ADD] Api integration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 4) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "totem_lodestar_apis", force: :cascade do |t|
+    t.string   "title"
+    t.string   "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "totem_lodestar_documents", force: :cascade do |t|
     t.string   "title"
